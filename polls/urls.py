b00d3c891f8b8567e7dc5f21e1d3b34dcfc08323
Raushan Kumar <raushan102189@gmail.com>
2022-05-09 07:14:32 +0530

    no idea
    hell or break

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.index, name='raj'),
+path('', views.index),
 path('details',views.details)
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def index(request):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    template = loader.get_template('index.html')
     # context = {
     # 'latest_question_list': latest_question_list,
     # }
